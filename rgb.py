fc78eb202ed84d9e29abbf917cea66831eab5029
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-03-19 20:53:51 +0000

    Add files via upload

--- rgb.py
+++ rgb.py
@@ -5,28 +5,31 @@ def defs(n):
     nn="0000"+nn[2:]
     nn=nn[-4:]
     n=""+nn[0]+nn[3]+nn[2]+nn[1]
-    return n
+    i=int(n,2)
+    return i
 def binss(n):
     nn=(n) & 15
     nn=bin(nn)
     nn="0000"+nn[2:]
     nn=nn[-4:]
     n=nn
-    return n
+    i=int(n,2)
+    return i
 
 def putc(c):
     if c!="":
         print(c[0],end="")
-def colors(colors:str):
-    i=str(int(colors,2)+40)
+def colors(colors:int):
+    i=str(colors+40)
     s="\033[$0m".replace("$0",i)
     print(s,end="")
 for a in range(8):
     colors(binss(a))
-    putc("*")
-colors("0000")
+    putc(" ")
+colors(0)
 print("\nrgb :",end="")
 for a in range(8):
     colors(defs(a))
-    putc("*")
+    putc(" ")
+colors(0)
 print("")
